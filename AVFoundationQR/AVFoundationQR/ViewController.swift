34bef70605cbba138e01ffae8139c67a1b64836e
ZIYOCHIRO <ziyochiro@gmail.com>
2018-11-27 16:07:17 +0800

    QR(quick responce) code reader

--- AVFoundationQR/AVFoundationQR/ViewController.swift
+++ AVFoundationQR/AVFoundationQR/ViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var previewView: UIView!
+    var sendURL: String!
+    var codeReader: CodeReader = AVCodeReader()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let previewLayer = codeReader.videoPreview
+        previewLayer.frame = previewView.bounds
+        previewView.layer.addSublayer(previewLayer)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.startReading), name: NSNotification.Name.NSExtensionHostWillEnterForeground, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.stopReading), name: NSNotification.Name.NSExtensionHostDidEnterBackground, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.startReading()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.stopReading()
+    }
+    
+    
+    @objc func startReading() {
+        codeReader.startReading(completion: didOutput)
+    }
+    @objc func stopReading() {
+        codeReader.stopReading()
+    }
+    
+    
+    private func didOutput(result: CodeReadResult) {
+        switch result {
+        case .success(let elemento): print(elemento)
+        case .failure: showNotAvaiableCameraError()
+        }
+    }
+    
+    private func showNotAvaiableCameraError() {
+        let alertController = UIAlertController(title: "Camera required", message: "This device has no camera. Is this an iOS Simulator?", preferredStyle: .alert)
+        let gotItAction = UIAlertAction(title: "Got it", style: .default, handler: nil)
+        alertController.addAction(gotItAction)
+        self.present(alertController, animated: false, completion: nil)
     }
 
 
