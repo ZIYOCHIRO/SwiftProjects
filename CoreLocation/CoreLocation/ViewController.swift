82cf37faacf010d8ac1477bd99cf44eb32763b1b
ZIYOCHIRO <ziyochiro@gmail.com>
2018-12-10 19:37:59 +0800

    core location

---
+++ CoreLocation/CoreLocation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CoreLocation
+//
+//  Created by 10.12 on 2018/12/10.
+//  Copyright © 2018 Rui. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    let location
+    @IBOutlet weak var myMapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
